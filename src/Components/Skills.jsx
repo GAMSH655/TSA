fa2e0eaf38da4d66811d01de61a0ad3735eb46ac
BIG_DEV360 <shittuabdquadri0@gmail.com>
2025-03-15 18:06:46 -0700

    roll up

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -7,6 +7,7 @@ import Market from "../assets/Market.png"
 import Planning from  "../assets/Planning.png"
 import Brand from "../assets/Branding.png"
 import Research from "../assets/research.png"
+import Public from "../assets/Public.png"
 const Skills = () => {
     const SKilsArray=[
         {skillsImg:Market, skillName:"business strategy"},
@@ -15,6 +16,8 @@ const Skills = () => {
         {skillsImg:Research, skillName:"research"},
         {skillsImg:Analytics, skillName:"business analysis"},
         {skillsImg:Brand, skillName:"Brand strategy"},
+        {skillsImg:Public, skillName:"public speaker"},
+        {skillsImg:Analysis, skillName:"market research"}
     ]
   return (
     <div className='mt-12'>
@@ -22,11 +25,11 @@ const Skills = () => {
         <div className="grid  grid-cols-1 md:grid-cols-2">
         {
           SKilsArray.map(({skillName, skillsImg},index)=>(
-            <div className="shadow-md p-3 m-3 rounded-md border-2 border-black  " key={index}>
+            <div className="shadow-lg p-3 m-3 rounded-md " key={index}>
              <div className="flex justify-center items-center">
              <img src={skillsImg} alt="" className='size-[100px]' />
              </div>
-              <p className="text-center font-semibold text-xl uppercase">{skillName}</p>
+              <p className="text-center font-semibold text-xl uppercase m-2">{skillName}</p>
             </div>
           ))
          }
